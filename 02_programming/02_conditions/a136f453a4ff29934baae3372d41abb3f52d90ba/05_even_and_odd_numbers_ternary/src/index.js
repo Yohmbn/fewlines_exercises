cfa8092977a94a852cd6280107394827857972e1
Yohan M <inuyasha59260@gmail.com>
2022-01-12 12:46:31 +0100

    05_even_and_odd_numbers_ternary

---
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/05_even_and_odd_numbers_ternary/src/index.js
@@ -0,0 +1,5 @@
+let numberToCheck = 18;
+
+// transform (or better, re do) the code of your previous exercise using a ternary operator.
+let msg = numberToCheck % 2 === 0 ? "The number is even" : "The number is odd";
+console.log(msg);
