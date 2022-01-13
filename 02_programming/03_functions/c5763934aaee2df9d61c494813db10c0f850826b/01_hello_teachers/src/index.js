fec7e0c174152704ae41d8a51675aeace762fc84
Yohan M <inuyasha59260@gmail.com>
2022-01-13 11:33:34 +0100

    01_hello_teachers

---
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/01_hello_teachers/src/index.js
@@ -0,0 +1,13 @@
+function sayHelloToTeachers(teachers) {
+  // Code the function here.
+  console.log ("Hello " + teachers[0] + "!");
+  console.log ("Hello " + teachers[1] + "!");
+  console.log ("Hello " + teachers[2] + "!");
+  console.log ("Hello " + teachers[3] + "!");
+  console.log ("Hello " + teachers[4] + "!");
+
+}
+
+// Do not remove last lines, it is for tests
+const spartaTeachers = ["Thomas", "Fenn", "Florian", "Martin", "Louis"];
+module.exports = sayHelloToTeachers;
