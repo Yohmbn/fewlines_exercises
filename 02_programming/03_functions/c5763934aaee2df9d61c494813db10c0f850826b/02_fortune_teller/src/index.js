3cfb226b4073fa6410ef523bf6fd2f7be3cee4b3
Yohan M <inuyasha59260@gmail.com>
2022-01-13 12:06:29 +0100

    02_fortune_teller

---
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
@@ -0,0 +1,7 @@
+function tellFortune(X,Y,Z,N) {
+  // Code the function here.
+  console.log("You will be " + X +" in " + Y + ", and married to " + Z +" in " + N + " years.");
+}
+
+// Do not remove last lines, it is for tests
+module.exports = tellFortune;
