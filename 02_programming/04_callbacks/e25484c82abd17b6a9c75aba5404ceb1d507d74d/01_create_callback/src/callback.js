c8ad71b304994fe8686260ca06092040f41a79b0
Yohan M <inuyasha59260@gmail.com>
2022-01-14 11:01:37 +0100

    01_create_callback

---
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/01_create_callback/src/callback.js
@@ -0,0 +1,15 @@
+// code here
+const sayHello = (player) => {
+  console.log("Hello, " + player.name);
+};
+
+// leave the code below for tests to work
+module.exports = sayHello;
+
+// const square = function (num) {
+//   return num * num;
+// };
+
+// const square = (num) => {
+//   return num * num;
+// };
