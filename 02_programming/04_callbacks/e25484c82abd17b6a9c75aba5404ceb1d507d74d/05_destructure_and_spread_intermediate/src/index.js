d57e5b870debeeb21869313724edc234ea8846da
Yohan M <inuyasha59260@gmail.com>
2022-01-14 15:51:15 +0100

    05_destructure_and_spread_intermediate

---
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/05_destructure_and_spread_intermediate/src/index.js
@@ -0,0 +1,23 @@
+// Here are some examples
+
+let storageHell = [
+  [],
+  ["IPad", "IPhone"],
+  ["GameBoy color"],
+  ["Nes", "Donkey kong 64"],
+  ["Apex Legends Starter Pack", "LG 5K 27p screen"],
+  ["Coffee Machine", "Azelad"],
+];
+// Should be cleaned this way => ['IPad', 'IPhone', 'GameBoy color', 'Nes', 'Donkey kong 64', 'Apex Legends Starter Pack', 'LG 5K 27p screen', 'Coffee Machine', 'Azelad']
+
+function formatStorage(x) {
+  const [a, b, c, d, e, f, ...g] = x;
+  const formaté = [...a, ...b, ...c, ...d, ...e, ...f, ...g];
+  return formaté;
+}
+
+// ↓ Feel free to uncomment this next line, or even create more to try everything you do ↓
+console.log(formatStorage(storageHell));
+
+// Don't modify this, it is for the tests.
+module.exports = formatStorage;
