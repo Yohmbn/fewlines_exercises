cd6d3258efa429bbaab77199f7b61b4f207280d7
Yohan M <inuyasha59260@gmail.com>
2022-01-25 12:22:46 +0100

    07_checking_a_spacecraft

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
@@ -1,5 +1,22 @@
-// Code here
+const stealthyFuelLevelCheck = (x) => {
+  if (x > 20) {
+    return "Fuel level: green";
+  } else if (x > 10 && x <= 20) {
+    return "Fuel level: yellow";
+  } else {
+    return "Fuel level: red";
+  }
+};
 
+const stealthyCargoHoldCheck = (x) => {
+  if (x.length === 4) {
+    return "Full";
+  } else if (x.length < 4) {
+    return `Spaces available: ${4 - x.length}`;
+  } else if (x.length > 4) {
+    return `Over capacity by ${x.length - 4} items.`;
+  }
+};
 // Do not remove last lines, it is for tests
 
 module.exports = { stealthyFuelLevelCheck, stealthyCargoHoldCheck };
