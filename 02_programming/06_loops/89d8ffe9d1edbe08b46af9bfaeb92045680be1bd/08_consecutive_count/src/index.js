4db58704418fdc904ac8bf2cf0102c20fd2b4c07
Yohan M <inuyasha59260@gmail.com>
2022-01-25 13:19:30 +0100

    08_consecutive_count

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/08_consecutive_count/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/08_consecutive_count/src/index.js
@@ -1,6 +1,24 @@
-function consecutiveCount() {
-  console.log(0);
+function consecutiveCount(a = "", b = "") {
+  let c = 0;
+  let d = 0;
+  let streak = "";
+  for (let i = 0; i < a.length; i++) {
+    while (a[i] === b) {
+      c = c + 1;
+      i = i + 1;
+    }
+    if (c > d) {
+      d = c;
+      c = 0;
+    } else {
+      c = 0;
+    }
+  }
+
+  return d;
 }
+
+console.log(consecutiveCount("utuutuuuuutuu", "u"));
 
 // Do not remove the following line, it is for tests
 module.exports = consecutiveCount;
