95574e7d76168db33fbd6a4499cb5734b854af7e
Yohan M <inuyasha59260@gmail.com>
2022-01-31 11:26:58 +0100

    03_typed_function_with_type_alias

---
+++ 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
@@ -0,0 +1,17 @@
+type MyTuple = [string,number];
+
+
+function handleTuple( tuple : MyTuple ): void{
+  if (typeof tuple[0]=== "string"){
+  console.log( `${tuple[0]} is a string`);
+  }
+  if(typeof tuple[1]==="number"){
+  console.log(`${tuple[1]} is a number`);
+  }
+  // Code the function here.
+}
+
+console.log(handleTuple(["foo",42]))
+
+// Leave the line below for tests to work properly.
+export { handleTuple };
