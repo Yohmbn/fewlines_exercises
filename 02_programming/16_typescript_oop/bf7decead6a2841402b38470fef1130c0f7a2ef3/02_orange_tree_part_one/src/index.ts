8708d277345931cc266c01a2e47fdcb8687ba763
Yohan M <inuyasha59260@gmail.com>
2022-02-01 11:00:56 +0100

    02_orange_tree_part_one

---
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
@@ -0,0 +1,24 @@
+class Tree {
+  age: number;
+  height: number;
+  alive : boolean = true;
+
+  constructor( age : number) {
+    this.age = age;
+    if (age < 10){
+      this.height = age * 25
+    } else if (age > 9 && age < 21){
+      this.height = 225 + (age - 9)*10
+    }
+
+  }
+  seed(): void  {
+    this.age = 0;
+    this.alive = true;
+    this.height = 0;
+
+  }
+}
+
+// Leave the line below for tests to work properly.
+export { Tree };
