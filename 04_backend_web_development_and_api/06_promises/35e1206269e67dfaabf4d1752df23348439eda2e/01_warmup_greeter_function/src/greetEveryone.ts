4fb9a3df50db959263dd9b7cf5c320282fd96ac2
Yohan M <inuyasha59260@gmail.com>
2022-02-21 09:50:14 +0100

    01_warmup_greeter_function

---
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/01_warmup_greeter_function/src/greetEveryone.ts
@@ -0,0 +1,8 @@
+type ParamRest = string[];
+
+export function greetEveryone(...rest : ParamRest): void {
+  for (let index = 0; index < rest.length; index++) {
+    console.log(`Welcome to ${rest[index]}`)
+    // Code your function here
+  }
+}
